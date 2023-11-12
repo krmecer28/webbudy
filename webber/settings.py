b3a299df5ad7bbb31235a8ee85d01402a3c91cd2
krmecer28 <ecerkerem28@gmail.com>
2023-11-13 00:22:01 +0300

    database

--- webber/settings.py
+++ webber/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'webber.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgres://postgres:eaG1Eg6-e22GddgCf21ed5g*bGDf26be@roundhouse.proxy.rlwy.net:37128/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'eaG1Eg6-e22GddgCf21ed5g*bGDf26be',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': 37128,
     }
 }
 
